daf04b2b51416b461083e3142cc41d7c2ed3bbb8
zakipon3 <miyapon3@gmail.com>
2020-05-29 12:14:56 +0900

    var calc

--- practice.php
+++ practice.php
@@ -1,4 +1,18 @@
 <?php
 $msg = "hello";
-var_dump( $msg );
+var_dump($msg);
+$msg = 7;
+var_dump($msg);
+$msg += 8;
+var_dump($msg);
+$msg -= 12;
+var_dump($msg);
+$msg *= 8;
+var_dump($msg);
+$msg /= 2;
+var_dump($msg);
+$msg %= 5;
+var_dump($msg);
+$msg **= 4;
+var_dump($msg);
 ?>

77e5151d5d03178b04ce06848f009346f8e82646
zakipon3 <miyapon3@gmail.com>
2020-05-30 14:23:03 +0900

    if

--- practice.php
+++ practice.php
@@ -1,10 +1,11 @@
 <?php
-var_dump("入力せよ");
-$msg = trim(fgets(STDIN));
-$price = trim(fgets(STDIN));
-var_dump($price.$msg);
-var_dump($price + $msg);
-
-var_dump("hello!", $msg, $price);
-var_dump("hello!");
+var_dump("年齢は？");
+(int)$age = trim(fgets(STDIN));
+if($age > 20){
+  var_dump($age."歳");
+  var_dump("成人です");
+}else{
+  var_dump("20歳以下です");
+  var_dump("未成年です");
+}
 ?>

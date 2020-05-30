abd1eaa0da58ff3ab97781b7e69218bab2b665d0
zakipon3 <miyapon3@gmail.com>
2020-05-30 14:28:59 +0900

    else if

--- practice.php
+++ practice.php
@@ -1,11 +1,14 @@
 <?php
 var_dump("年齢は？");
 (int)$age = trim(fgets(STDIN));
-if($age > 20){
-  var_dump($age."歳");
-  var_dump("成人です");
-}else{
+if($age < 20){
   var_dump("20歳以下です");
   var_dump("未成年です");
+}elseif($age < 65){
+  var_dump($age."です");
+  var_dump("成人です");
+}else{
+  var_dump($age."歳");
+  var_dump("高齢者です");
 }
 ?>

82991809adb15c823b3230defd447e272c207573
zakipon3 <miyapon3@gmail.com>
2020-05-30 11:07:52 +0900

    string int difference

--- practice.php
+++ practice.php
@@ -1,22 +1,7 @@
 <?php
-$msg = "hello";
-var_dump($msg);
-$msg = 7;
-var_dump($msg);
-$msg += 8;
-var_dump($msg);
-$msg -= 12;
-var_dump($msg);
-$msg *= 8;
-var_dump($msg);
-$msg /= 2;
-var_dump($msg);
-$msg %= 5;
-var_dump($msg);
-$msg **= 4;
-var_dump($msg);
-
 var_dump("入力せよ");
-$msg = fgets(STDIN);
-var_dump($msg);
+$msg = trim(fgets(STDIN));
+$price = trim(fgets(STDIN));
+var_dump($price.$msg);
+var_dump($price + $msg);
 ?>

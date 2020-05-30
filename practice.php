40a54c638eaa8252c649ef0768468485b8760e69
zakipon3 <miyapon3@gmail.com>
2020-05-30 16:38:54 +0900

    return

--- practice.php
+++ practice.php
@@ -1,10 +1,7 @@
 <?php
-$team = ["A", "B", "C", "D", "E"];
-$opps = ["A", "B", "C", "D", "E"];
-foreach($team as $a){
-  array_shift($opps);
-  foreach($opps as $b){
-    var_dump($a."対".$b);
-  }
+function createmsg($price){
+  return 500 * $price;
 }
+
+var_dump("代金は".createmsg(6). "円です。");
 ?>
